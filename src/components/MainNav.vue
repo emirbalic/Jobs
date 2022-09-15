b64b8ec46cc0f4563600eff87a168394a0a3307d
Emir <emirbalic@gmail.com>
2022-09-15 13:45:34 +0200

    Some Directives

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,17 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-grey-1">
-        <a href="/" class="flex items-center h-full text-xl">{{company}}</a>
+        <a :href="url" class="flex items-center h-full text-xl">{{company}}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li 
+              v-for="menuItem in menuItems" 
+              :key="menuItem" 
+              class="h-full ml-9 first:ml-0"  
+              data-test='main-nav-list-item'
+             ><a href="#" class="flex items-center h-full py-2 5">{{menuItem}}</a></li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -14,6 +24,8 @@ export default {
   data() {
     return {
       company:'Kuka Alenami',
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Locations', 'Life at MDB', 'How we hire', 'Students', 'Jobs']
     }
   }
 };
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,11 +1,29 @@
 import { mount } from "@vue/test-utils";
 
-import MainNav from '@/components/MainNav.vue';
+import MainNav from "@/components/MainNav.vue";
 
-describe(('MainNav'), () => {
-  it('displays company name', () => {
+describe("MainNav", () => {
+  it("displays company name", () => {
     const wrapper = mount(MainNav);
-    expect(wrapper.text()).toMatch('Kuka Alenami');
+    expect(wrapper.text()).toMatch("Kuka Alenami");
+  });
+  it("displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    // const navigationMenuItem = wrapper.find('li');
+    // const navigationMenuItems = wrapper.findAll('li');
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    // console.log(navigationMenuTexts);
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at MDB",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
   });
 });
 
